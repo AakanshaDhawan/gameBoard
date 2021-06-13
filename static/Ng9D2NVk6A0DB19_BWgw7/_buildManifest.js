17555972b93395aeaafd26b2cf04f3178012ad33
aakanshaMobiux <aakansha@mobiux.in>
2021-06-13 18:43:07 +0530

    Updates

---
+++ static/Ng9D2NVk6A0DB19_BWgw7/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a,b){return {"/":[a,b],"/about":[a,b],"/corporate_events":[a,b],"/index":[a,b],"/live_events":[a,b]}}("static\u002Fchunks\u002Fa9a7754c.97b611add67558c1a759.js","static\u002Fchunks\u002F1168d3e8ffb8cb0074d9b0e06e6ef52b69b32751.ac1771cab7bb06e25815.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
---
+++ static/tVY1wJJRfZYLN2x8UtiA9/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a,b){return {"/":[a,b],"/about":[a,b],"/corporate_events":[a,b],"/index":[a,b],"/live_events":[a,b]}}("static\u002Fchunks\u002Fa9a7754c.97b611add67558c1a759.js","static\u002Fchunks\u002F771c93839163a65d8fa2d5a4caedd691c1d149ea.ac1771cab7bb06e25815.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
