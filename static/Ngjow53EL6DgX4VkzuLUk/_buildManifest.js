39005a8629a1f4f12143a13680847dd7aa503df6
aakanshaMobiux <aakansha@mobiux.in>
2021-06-13 18:12:25 +0530

    Updates

---
+++ static/Ngjow53EL6DgX4VkzuLUk/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a,b){return {"/":[a,b],"/about":[a,b],"/corporate_events":[a,b],"/index":[a,b],"/live_events":[a,b]}}("static\u002Fchunks\u002Fa9a7754c.0982642c13c98158e5e2.js","static\u002Fchunks\u002Fb52d50f066210f2b0039d22db95cbd7583de8da3.58e2806c10fa4b5f2246.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
